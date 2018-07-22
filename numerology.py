dfc674b5a48cd9da14400a038c8d5eedacbf8709
Agha Awais <34622842+aghaPathan@users.noreply.github.com>
2018-07-22 16:20:36 +0500

    Update numerology.py
    added method to calculate lucky number based on birth date

--- numerology.py
+++ numerology.py
@@ -1,31 +1,39 @@
-class Numerology():
-    """
-            This class takes input as string of a name and output a numerological number
-        """
+class Numerology:
+
     def __init__(self):
-        self.name_result = int()
-        self.num_result = int()
+
+        self.total = int()
         self.numbers = {1: 'ajs', 2: 'bkt', 3: 'clu', 4: 'dmv', 5: 'enw', 6: 'fox', 7: 'gpy', 8: 'hqz', 9: 'ir'}
-        self.temp = int()
 
-    def name(self,input):
-        name = input.replace(' ', '').lower()
 
+    def name(self, name):
+       """ This method will process the name """ 
+        name = name.replace(' ', '').lower()
+        name_result = 0
         for letter in name:
-            for key, value in self.numbers.iteritems():
+            for key, value in self.numbers.items():
                 if letter in value:
-                    self.name_result += key
+                    name_result += key
 
-        return self.sum(str(self.name_result))
+        self.add(str(name_result))
 
-    def sum(self, input):
-        i = 0
-        self.num_result = 0
-        while i <= len(input)-1:
-            self.num_result += int(input[i])
-            i += 1
-            if i > len(input)-1:
-                if len(str(self.num_result)) > 1:
-                    return self.sum(str(self.num_result))
-                if len(str(self.num_result)) == 1:
-                    return self.num_result
+        return self.total
+
+    def add(self,number):
+        """ Addition Logic """
+        total = 0
+        for n in number:
+            total += int(n)
+        if len(str(total)) > 1:
+            self.add(str(total))
+        else:
+            # Final result
+            self.total = total
+
+
+    def birth_date(self,date):
+        """  Input date with dd/mm/yyyy format  """
+        date = date.replace('/','')
+        self.add(date)
+        return self.total
+    
